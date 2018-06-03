436c792b357cc0b570afb1b29ec655ea51925dc7
Jonathan Queiroz <dev@johnjq.com>
2018-06-03 20:21:45 -0300

    Implement solution in C++

---
+++ main.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct State {
+    string to_string() const;
+    string to_readable_string() const;
+    static bool populate_from_string(State& state, const string& str);
+    bool is_goal() const;
+
+    array<array<int, 3>, 3> grid;
+};
+
+string State::to_string() const {
+    string str;
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            str.push_back('0'+grid[i][j]);
+    return str;
+}
+
+string State::to_readable_string() const {
+    string str;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            str.push_back(grid[i][j] == 0 ? '.' : '0'+grid[i][j]);
+        }
+        str.push_back('\n');
+    }
+    return str;
+}
+
+bool State::populate_from_string(State& state, const string& str) {
+    if (str.size() != 9)
+        return false;
+    for (int i = 0; i < 9; ++i) {
+        if (str[i] < '0' || str[i] > '9')
+            return false;
+        state.grid[i / 3][i % 3] = str[i] - '0';
+    }
+    return true;
+}
+
+bool State::is_goal() const {
+    array<array<int, 3>, 3> goal_grid{
+        array<int, 3>{0, 1, 2},
+        array<int, 3>{3, 4, 5},
+        array<int, 3>{6, 7, 8}
+    };
+    return grid == goal_grid;
+}
+
+bool operator<(const State& lhs, const State& rhs) {
+    return lhs.grid < rhs.grid;
+}
+
+vector<State> get_neighbors(const State& state) {
+    int pi = -1, pj = -1;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (state.grid[i][j] == 0) {
+                pi = i;
+                pj = j;
+            }
+        }
+    }
+    if (pi == -1) {
+        throw std::runtime_error("invalid state: missing space");
+    }
+    const vector<pair<int, int>> displacements{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vector<State> neighbors;
+    for (const auto& displacement : displacements) {
+        const int new_pi = pi + displacement.first;
+        const int new_pj = pj + displacement.second;
+        if (0 <= new_pi && new_pi < 3 && 0 <= new_pj && new_pj < 3) {
+            State new_state(state);
+            swap(new_state.grid[pi][pj], new_state.grid[new_pi][new_pj]);
+            neighbors.push_back(new_state);
+        }
+    }
+    return neighbors;
+}
+
+struct Node {
+    const Node *const parent;
+    const State state;
+    const int distance;
+};
+
+void solve(const State& initial_state) {
+    vector<unique_ptr<Node>> node_pool;
+    Node *initial_node = new Node{nullptr, initial_state, 0};
+    node_pool.emplace_back(initial_node);
+    Node *final_node = nullptr;
+    if (initial_state.is_goal()) {
+        final_node = initial_node;
+    } else {
+        set<State> visited;
+        queue<Node*> q;
+        visited.insert(initial_node->state);
+        q.push(initial_node);
+        while (!q.empty()) {
+            Node *node = q.front();
+            q.pop();
+
+            for (const State& new_state : get_neighbors(node->state)) {
+                if (visited.insert(new_state).second) {
+                    Node *new_node = new Node{node, new_state, node->distance+1};
+                    node_pool.emplace_back(new_node);
+                    if (new_state.is_goal()) {
+                        final_node = new_node;
+                        break;
+                    }
+                    q.push(new_node);
+                }
+            }
+
+            if (final_node != nullptr) {
+                break;
+            }
+        }
+    }
+    // Print response
+    if (final_node == nullptr) {
+        cout<<"No solution."<<endl;
+    } else {
+        cout<<"Solution in "<<final_node->distance<<" step(s)."<<endl;
+    }
+}
+
+int32_t main() {
+    string line;
+    cin>>line;
+    State initial_state;
+    if (!State::populate_from_string(initial_state, line)) {
+        cerr<<"Invalid input."<<endl;
+        return 1;
+    }
+    solve(initial_state);
+}
