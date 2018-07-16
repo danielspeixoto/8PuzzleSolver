e91c9cfa7d5c8ad477d9c5abacbf7c357fb8b307
Jonathan Queiroz <dev@johnjq.com>
2018-07-16 16:56:05 -0300

    Log A* results

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
 
-echo "Compiling C++ solution..."
-g++ -std=c++11 -O2 main.cpp -o main
+if [[ "$LANGUAGE" == "cpp" ]]; then
+    echo "Compiling C++ solution..."
+    g++ -std=c++11 -O2 main.cpp -o main
+fi
 
 echo "Solving instances..."
 while read line; do
     echo "----------------"
     echo "Input: $line"
-    OUT=$(echo "$line" | ./main $*)
+    if [[ "$LANGUAGE" == "cpp" ]]; then
+        OUT=$(echo "$line" | ./main $*)
+    else
+        OUT=$(echo "$line" | ./a.py $*)
+    fi
     echo "$OUT"
 done < some_grids
